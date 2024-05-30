a9cfa5d541a67e44f5757e4ea00a0e04228fbdf2
nabiny501 <nabiny501@gmail.com>
2024-05-30 18:57:28 +0000

    react-learn init

---
+++ src/navbar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+        <div className="navbar">
+            <a href="/">Home</a>
+            <a href="/products">Projects</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </div>
+    )
+} 
